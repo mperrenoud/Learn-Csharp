1ef990f5b8737a557f6ecf0a7f3ce520a3dc439e
mperren@gmail.com <1God&not3>
2014-05-26 19:38:53 -0400

    Initial commit.

---
+++ LCS101-001/LCS101-001/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCS101_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
